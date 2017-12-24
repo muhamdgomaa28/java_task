4ab75ecb24067f91e002eb454e9afb6405b18088
muhamdgomaa28 <muhamdgomaa28@gmail.com>
2017-12-24 01:29:50 -0800

    first

---
+++ src/main/java/threads_operation.java
@@ -0,0 +1,206 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.Statement;
+
+class ThreadSynchronization
+
+{
+//   public static StringBuffer insertbuffer = new StringBuffer();
+//   static StringBuffer updatebuffer = new StringBuffer();
+//    static StringBuffer createbuffer = new StringBuffer();
+//
+//    public static void main(String args[])
+//
+//    {
+//        /// read file and seprate it to make three threads depends on query type
+//
+//        String s            = new String();
+//        // contain all file
+//        StringBuffer sb = new StringBuffer();
+//       // StringBuilder sb= new StringBuilder();
+//
+//
+//
+//
+//        try
+//        {
+//            FileReader fr = new FileReader(new File("E:\\javatask\\DigitalWellnez_Java_trial\\SQL\\statements.sql"));
+//            // be sure to not have line starting with "--" or "/*" or any other non aplhabetical character
+//
+//            BufferedReader br = new BufferedReader(fr);
+//
+//            while((s = br.readLine()) != null)
+//            {
+//                sb.append(s);
+//            }
+//            br.close();
+//
+//            // here is our splitter ! We use ";" as a delimiter for each request
+//            // then we are sure to have well formed statements
+//            String[] inst = sb.toString().split(";");
+//
+//            for(int i = 0; i<inst.length; i++)
+//            {
+//
+//                if(inst[i].trim().toLowerCase().startsWith("insert"))
+//                {
+//insertbuffer.append(inst[i]+";");
+//                }else if(inst[i].trim().toLowerCase().startsWith("update")){
+//                   updatebuffer.append(inst[i]+";");
+//                }else if(inst[i].trim().toLowerCase().startsWith("create")){
+//
+//                    createbuffer.append(inst[i]+";");
+//                }
+//            }
+//
+//
+//        }
+//        catch(Exception e)
+//        {
+//e.printStackTrace();
+//        }
+//////
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//       // make  three threds classes each one excute one type of query
+//
+//        insertThread thread1 = new insertThread();
+//
+//        updateThred thread2 = new updateThred();
+//
+//        createThred thread3 =new createThred();
+//
+//        thread1.start();
+//
+//        thread2.start();
+//
+//        thread3.start();
+//
+//        boolean thread1IsAlive = true;
+//
+//        boolean thread2IsAlive = true;
+//
+//        boolean thread3IsAlive = true;
+//
+//
+//        do {
+//
+//            if (thread1IsAlive && !thread1.isAlive()) {
+//
+//                thread1IsAlive = false;
+//
+//                System.out.println("Thread 1 is dead.");
+//
+//            }
+//
+//            if (thread2IsAlive && !thread2.isAlive()) {
+//
+//                thread2IsAlive = false;
+//
+//                System.out.println("Thread 2 is dead.");
+//
+//            }
+//            if (thread3IsAlive && !thread3.isAlive()) {
+//
+//                thread3IsAlive = false;
+//
+//                System.out.println("Thread 3 is dead.");
+//
+//            }
+//
+//        } while(thread1IsAlive || thread2IsAlive||thread3IsAlive);
+//
+//    }
+//
+//
+//
+//}
+}
+//////////////end /////////
+class insertThread extends Thread
+
+{
+
+    public void run()
+
+    {
+
+
+        SynchronizedOutput.displayList(Main.insertbuffer);
+
+    }
+
+}
+
+/////////////second thread class ////////////////////////////
+class updateThred extends Thread
+
+{
+
+    public void run()
+
+    {
+
+        SynchronizedOutput.displayList(Main.updatebuffer);
+
+    }
+
+}
+class createThred extends Thread
+
+{
+
+
+    public void run()
+
+    {
+
+        SynchronizedOutput.displayList(Main.createbuffer);
+
+    }
+
+
+}
+
+
+////////////// end //////
+class SynchronizedOutput
+
+{
+
+    public static synchronized void displayList(StringBuffer name)
+
+    {
+
+        String[] inst = name.toString().split(";");
+try {
+    Connection c = database.getConnection();
+    Statement st = c.createStatement();
+
+    for (int i = 0; i < inst.length; i++) {
+
+        if (!inst[i].trim().equals("")) {
+            st.executeUpdate(inst[i]);
+            System.out.println(">>" + inst[i]);
+        }
+    }
+
+
+}catch (Exception e){
+    e.printStackTrace();
+}
+
+    }
+
+}
